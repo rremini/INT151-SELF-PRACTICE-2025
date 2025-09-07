b1d85393505fb7b5681f6a3ef9e835cd7e5ad77e
Chayanon Maksakarn <rremini@users.noreply.github.com>
2025-09-07 17:33:30 +0700

    Add all files in local and Submit Week 1-5

--- self-practice/week03/function-practice.js
+++ self-practice/week03/function-practice.js
@@ -1,15 +1,15 @@
-/*
-Write a fucntion
-*/
-
 function getFreqOfWords(sententce) {
     if (typeof sententce !== 'string') {
         return undefined
     } 
-    const words = sententce.toLowerCase().trim().split(/\s+/);
-    for (i = 0; i < words; i++) {
-        console.log({sententce : i})
-    } return sententce
+    const words = sententce
+    .toLowerCase()
+    .trim()
+    .split(/\s+/);
+    return words.reduce((acc, word) => {
+    acc[word] = (acc[word] || 0) + 1;
+    return acc;
+  }, {});
 }  
 
 console.log(getFreqOfWords('Today is present and present is your gift'))
